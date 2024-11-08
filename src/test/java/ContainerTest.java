d633eecda7eddc4f0d4005979b6d986434a4717a
Andrey Morokhin <morokhin.andrei@gmail.com>
2024-11-08 13:33:35 +0500

    Part solution

---
+++ src/test/java/ContainerTest.java
@@ -0,0 +1,36 @@
+import example.container.Container;
+import example.container.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *  Тестирование класса Container
+ */
+public class ContainerTest {
+
+    private Container container;
+
+    /**
+     *  Очистка container путем создания нового
+     */
+    @BeforeEach
+    public void setUp() {
+        container = new Container();
+    }
+
+    /**
+     *  Проверка на добавление Item
+     */
+    @Test
+    public void addItemTest() {
+
+    }
+
+    /**
+     *  Проверка на удаление Item
+     */
+    @Test
+    public void deleteItemTest() {
+
+    }
+}
